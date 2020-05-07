836462a2397b518a1ffe0853ee54a2f035a3e448
evgenval <34027247+evgenval@users.noreply.github.com>
2020-05-07 21:10:40 +0300

    Added Lois lab1 & lab2

---
+++ Lois/lab1/lois-1/script.js
@@ -0,0 +1,408 @@
+//@author Шамрук Евгений
+//@group 721701
+//@year 2020
+//@subject ЛОИС
+//@title Лабораторная работа N1
+/*@task D: Проверить является ли строка формулой СДНФ.*/
+
+//Включение строгого стандарта написания js скриптов
+"use strict";
+
+//Опредеоение константных переменных
+const FORMULA_ID = "formula";
+const ANSWER_1_ID = "answer1";
+const FORMULA_REGEXP = new RegExp('([(]([A-Z]|[0-1])((->)|(&)|(\\|)|(~))([A-Z]|[0-1])[)])|([(][!]([A-Z]|[0-1])[)])|([A-Z])|([0-1])','g');
+const CONTAINER_ID = "container";
+const FORMULA_LABEL_ID = "formulaLabel";
+const TABLE_ID = "table";
+
+
+const NEGATION = "!";
+const CONJUNCTION = "&";
+const DISJUNCTION = "|";
+const IMPLICATION = "->";
+const EQUIVALENCE = "~";
+
+//Переменные, используемые для расчета таблицы истинности
+let countAnswer = 0;
+let n = 1;
+
+//Стартовая фунция запуска скрипта
+function run() {
+    let formula = document.getElementById(FORMULA_ID).value;
+    checkSDNF(formula);
+    document.getElementById(CONTAINER_ID).hidden = true;
+    document.getElementById(FORMULA_LABEL_ID).innerHTML = formula;
+
+    let obj = calculateTableTruth(formula);
+
+
+    if (obj != null && checkWithRegularExpressionFormula(formula)) {
+        printTableTruth(obj.table, obj.symbolSize);
+        document.getElementById(CONTAINER_ID).hidden = false;
+        }
+
+}
+
+function checkSDNF(expression) {
+    if (expression != "") {
+        if (expression.length == 1) {
+            let reg = /[A-Z]/g
+            if (reg.test(expression)) {
+                document.getElementById(ANSWER_1_ID).innerHTML = 'Введенная формула является СДНФ';
+            } else {
+                document.getElementById(ANSWER_1_ID).innerHTML = 'Введенная формула не является СДНФ';
+            }
+        } else {
+            let isFormula = checkWithRegularExpressionFormula(expression);
+            let totalOperations = expression.match(/&|\||->|!|~/g);
+            let numOfOperations = totalOperations.length;
+            if (isFormula) {
+                let boolResult = checkAllConjunctions(expression, '|');
+                if (expression.match(/((!)[(])/)) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = 'Введенная формула не является СДНФ, так как содержит двойные или групповые отрицания';
+                    return;
+                }
+                if (boolResult) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = 'Введенная формула является СДНФ';
+                } else {
+                    document.getElementById(ANSWER_1_ID).innerHTML = 'Введенная формула не является СДНФ';
+                }
+            } else {
+                if (expression.match(/\(|\)/g) == null) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = "Данное выражение не является логической формулой, так как отсутствуют скобки";
+                }
+                let allBrackets = expression.match(/\(|\)/g);
+                //Проверяем на наличие лишних символов
+                if (expression.match(/(?!([A-Z]|&|\||\(|\)|!))./g)) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = "Данное выражение не является логической формулой, так как содержит неправильные символы";
+                }
+                //Проверяем на чётность количества скобок
+                if (!(allBrackets.length % 2 == 0)) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = "Данное выражение не является логической формулой, так как количество открывающихся и закрывающихся скобок не совпадают";
+                }
+                if (allBrackets.length / 2 != numOfOperations.length) {
+                    document.getElementById(ANSWER_1_ID).innerHTML = "В выражении есть лишние скобки";
+                }
+
+                alert('Данное выражение не является логической формулой');
+            }
+        }
+    } else {
+        document.getElementById(ANSWER_1_ID).innerHTML = 'Строка пуста';
+    }
+}
+
+/**
+ @author Vladislav Shusterman
+ @group 621701
+ */
+function checkAllConjunctions(expression, sep) {
+    let conjunctionOperationsArray = expression.split(sep);
+    if (conjunctionOperationsArray == false) return false
+    let j, k;
+    let conj = '&';
+    let resBooltrue = false;
+    let resBoolfalse = false;
+    let eqResBool = false;
+    for (j = 0; j < conjunctionOperationsArray.length; j++) {
+        for (k = 0; k < conjunctionOperationsArray.length; k++) {
+            if (j != k) {
+                resBooltrue = compareConjunctions(conjunctionOperationsArray[j].split(conj), conjunctionOperationsArray[k].split(conj), true);
+                resBoolfalse = compareConjunctions(conjunctionOperationsArray[j].split(conj), conjunctionOperationsArray[k].split(conj), false);
+                if (resBooltrue == false || resBoolfalse == false) return false
+            } else {
+                eqResBool = compareSingleConjunction(conjunctionOperationsArray[j].split(conj));
+                if (eqResBool == false) return false
+            }
+        }
+    }
+    return true
+}
+
+/**
+ @author Vladislav Shusterman
+ @group 621701
+ */
+function compareConjunctions(first, second, type) {
+    console.log(first, second, type)
+    let x, y;
+    let bool;
+    if (first.length != second.length) return false
+    for (x = 0; x < first.length; x++) {
+        bool = false;
+        for (y = 0; y < second.length; y++) {
+            if (type) {
+                first[x] = first[x].replace(/[!()]/g, '');
+                second[y] = second[y].replace(/[!()]/g, '');
+            } else {
+                first[x] = first[x].replace(/[()]/g, '');
+                second[y] = second[y].replace(/[()]/g, '');
+            }
+            if (first[x] == second[y]) {
+                bool = true;
+                break;
+            }
+        }
+        if (type) {
+            if (!bool) return false
+        } else {
+            if (bool == false) return true
+        }
+    }
+    return type;
+}
+
+/**
+ @author Vladislav Shusterman
+ @group 621701
+ */
+function compareSingleConjunction(first) {
+    let x, y;
+    let bool;
+    for (x = 0; x < first.length; x++) {
+        bool = true
+        for (y = 0; y < first.length; y++) {
+            if (x != y) {
+                first[x] = first[x].replace(/[!()]/g, '');
+                first[y] = first[y].replace(/[!()]/g, '');
+                if (first[x] == first[y]) return false;
+            }
+        }
+        if (bool != true) return false;
+    }
+    return bool;
+}
+
+/**
+ @author Vladislav Shusterman
+ @group 621701
+ */
+function checkWithRegularExpressionFormula(formula) {
+    let form = formula;
+    /**
+     @author Alexandr Zhuk
+     @group 521701
+     */
+    if (form.length == 1 && form.match(/[A-Z]|[0-1]/)) {
+        return true;
+    } else {
+        while (true) {
+            let initLength = form.length;
+            form = form.replace(FORMULA_REGEXP, '1')
+            if (form.length === initLength) {
+                break;
+            }
+        }
+        if ((form.length === 1) && (form.match(/1/))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+//Функция рассчитывания таблицы истинности
+function calculateTableTruth(formula) {
+    countAnswer = 0;
+    n = 1;
+
+    if(formula == '0') {
+        countAnswer = 1;
+        return null;
+    }
+
+    if(formula == '1') {
+        return null;
+    }
+
+    if (formula == '') {
+        return null;
+    }
+
+    let answer = formula;
+    let symbolInFormula = calculateFormulaSymbols(formula).sort();
+    let sizeSymbolInFormula = symbolInFormula.length;
+    n = Math.pow(2, sizeSymbolInFormula);
+
+    let table = {};
+    for (let index = 0; index < n; index++) {
+        let inputParameters = calculateInputFormulaParameters(index, sizeSymbolInFormula);
+        let obj = createFormulaWithParameters(symbolInFormula, inputParameters);
+
+        obj[answer] = getAnswer(formula, obj);
+        table[index] = obj;
+
+        if (obj[answer] == 0) {
+            countAnswer++;
+        }
+    }
+
+    return  {
+        table: table,
+        symbolSize: sizeSymbolInFormula
+    };
+}
+
+//Функция нахождения количества символов в формуле
+function calculateFormulaSymbols(formula) {
+    const SYMBOL_REGEXP = new RegExp('([A-Z])', "g");
+    let results = formula.match(SYMBOL_REGEXP);
+
+    //удаляет повторяющиеся символы
+
+    for(let i = 0; i < results.length; i++) {
+        for(let j = i + 1; j < results.length; j++) {
+            if (results[i] == results[j]) {
+                results.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return results;
+}
+
+//Функция печати таблицы истинности
+function printTableTruth(table, symbolSize) {
+    let tableSize = Math.pow(2, symbolSize);
+    let html = "";
+
+    //построение шапки таблицы
+
+    html += "<tr>";
+
+    for (let key of Object.keys(table[0])) {
+        html += "<td>" + key + "</td>"
+    }
+
+    html += "</tr>";
+
+    //непосредственное заполнение
+
+    for (let index = 0; index < tableSize; index++) {
+        let object = table[index];
+        html += "<tr>";
+
+        for (let key of Object.keys(object)) {
+            html += "<td>" + object[key] + "</td>"
+        }
+        html += "</tr>";
+    }
+
+    let tableElement = document.getElementById(TABLE_ID);
+    tableElement.innerHTML = html;
+}
+
+//Функция расчета входных параметров для формулы
+function calculateInputFormulaParameters(index, symbolSize) {
+    let res = index.toString(2);
+    //дописывает 0, если не хватает разрядов
+    for (let index = res.length; index < symbolSize; index++) {
+        res = "0" + res;
+    }
+
+    return res;
+}
+
+//Создания объекта формулы со входными параметрами
+function createFormulaWithParameters(symbolInFormula, inputParameters) {
+    let object = {};
+    for (let index = 0; index < symbolInFormula.length; index++) {
+        let symbol = symbolInFormula[index];
+        //связь входного символа формулы с его входным значением
+        object[symbol] = inputParameters[index];
+    }
+
+    return object;
+}
+
+//Функция получения результата логической формулы
+function getAnswer(formula, obj){
+    let constFormula = formula;
+    for (let key of Object.keys(obj)) {
+        let value = obj[key];
+        //заменяем буквы значениями
+        constFormula = constFormula.replace(new RegExp(key, 'g'), value);
+    }
+    return calculateFormula(constFormula);
+}
+
+//Функция высчитывания результата логической формулы
+function calculateFormula(formula) {
+    const REGEXP = new RegExp("([(][" + NEGATION + "][0-1][)])|" + "([(][0-1]((" + CONJUNCTION + ")|("+ "\\" + DISJUNCTION + ")|(" + IMPLICATION + ")|(" + EQUIVALENCE + "))[0-1][)])");
+    while (REGEXP.exec(formula) != null) {
+        let subFormula = REGEXP.exec(formula)[0];
+        let result = calculateSimpleFormula(subFormula);
+        formula = formula.replace(subFormula, result);
+    }
+
+    return formula;
+}
+
+//Высчитывание простой формулы
+function calculateSimpleFormula(subFormula) {
+    if (subFormula.indexOf(NEGATION) > -1) {
+        return calculateNegation(subFormula);
+    }
+
+    if (subFormula.indexOf(CONJUNCTION) > -1) {
+        return calculateConjunction(subFormula);
+    }
+
+    if (subFormula.indexOf(DISJUNCTION) > -1) {
+        return calculateDisjunction(subFormula);
+    }
+
+    if (subFormula.indexOf(IMPLICATION) > -1) {
+        return calculateImplication(subFormula);
+    }
+
+    if (subFormula.indexOf(EQUIVALENCE) > -1) {
+        return calculateEquivalence(subFormula);
+    }
+}
+
+//Функция высчитывания отрицания
+function calculateNegation(subFormula) {
+    if (parseInt(subFormula[2]) == 1) {
+        return 0;
+    }
+    return 1;
+}
+
+//Функция высчитывания конъюнкции
+function calculateConjunction(subFormula) {
+    if (parseInt(subFormula[1]) && parseInt(subFormula[3])) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//Функция высчитывания дизъюнкции
+function calculateDisjunction(subFormula) {
+    if (parseInt(subFormula[1]) || parseInt(subFormula[3])) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//Функция высчитывания импликации
+function calculateImplication(subFormula) {
+    if ((!parseInt(subFormula[1])) || parseInt(subFormula[4])) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//Функция высчитывания эквиваленции
+function calculateEquivalence(subFormula) {
+    if (parseInt(subFormula[1]) == parseInt(subFormula[3])) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
---
+++ Lois/lab2/l2/script.js
@@ -0,0 +1,262 @@
+﻿//ЛР 2. Вариант 1. Вычислить возможные значения формулы при частично заданной интерпретации формулы
+//      (частично заданных значениях пропозициональных переменных).
+//      Результат оформить в виде таблицы.
+
+//Автор: Шамрук Е. В. Гр. 721701
+
+
+//Проверка является ли строка формулой логики высказываний
+function verificate(){
+	//var isCorrect = false;
+	
+    var inputStringVar = document.getElementById('inputString').value;
+
+	if(formulaCheck(inputStringVar)){
+		alert("Строка является формулой логики высказываний.");
+	} else {
+		alert("Строка НЕ является формулой логики высказываний или не содержит символов.");
+	}
+}
+
+function formulaCheck(inputStringVar){
+	if(inputStringVar.length == 1){
+		if(inputStringVar.match(/([0-1]|[A-Z])/)){
+			return true;
+		} else {
+			return false;
+		}
+	//унарная формула
+	} else if(inputStringVar.match(/^\(!([A-Z]|[0-1]|(\((\(|\)|[A-Z]|[0-1]|!|&|~|\||(->))+\)))\)$/)){
+		/*if(formulaCheck(inputStringVar.substring(2, inputStringVar.length - 1))){
+			return true;
+		} else {
+			return false;*/
+			return (formulaCheck(inputStringVar.substring(2, inputStringVar.length - 1)));
+		//}
+	//бинарная формула                 (первая подформула __________________________________)(связка_____)(вторая подформула __________________________________)
+	} else if(inputStringVar.match(/^\(([A-Z]|[0-1]|(\((\(|\)|[A-Z]|[0-1]|!|&|~|\||(->))+\)))(&|~|\||(->))([A-Z]|[0-1]|(\((\(|\)|[A-Z]|[0-1]|!|&|~|\||(->))+\)))\)$/)){
+		var subformulaStart = 0;
+		var lev = 0;//уровень вложенности
+		var max = inputStringVar.length - 2;
+		var subformualsCheckResults = [false, false];
+		var subformulaInd = 0;
+		var subformulaIndex = 0;
+		for(var i = 1; i <= max; i++){
+			if(inputStringVar.charAt(i) == '('){
+				if(lev == 0){
+					subformulaStart = i;
+				}
+				lev++
+			} else if (inputStringVar.charAt(i) == ')'){
+				lev--;
+				if(lev == 0){
+					subformualsCheckResults[subformulaInd] = formulaCheck(inputStringVar.substring(subformulaStart, i + 1));//крайний символ НЕ включается
+					subformulaInd++;					
+				}
+			} else if(lev == 0) {
+				if(inputStringVar.charAt(i).match(/([0-1]|[A-Z])/)){
+					subformualsCheckResults[subformulaInd] = true;
+					subformulaInd++;
+				}
+			}
+		}
+		if(subformualsCheckResults[0]&&subformualsCheckResults[0]){
+			return true;
+		} else return false;
+	}
+}
+
+function calculate(){
+	document.getElementById('TableBody').innerHTML = '';
+	document.getElementById('continueButtonDiv').innerHTML = '';
+	var inputStringVar = document.getElementById('inputString').value;
+	if(!formulaCheck(inputStringVar)){
+		alert("Строка НЕ является формулой логики высказываний или не содержит символов.");
+	} else {
+	var variablesArr = searchVariables(inputStringVar);
+	drawInputTable(variablesArr, inputStringVar);
+	//drawVariablesTable(variablesArr);//таблица для указания значений некоторых пееменных, добавить кнопку для продолжения
+	//var valueMatrix;//размерность зависит от количества заданных переменных
+	
+	//составить таблицу с возможными значениями переменных
+	//calculateFormulaValue, результат добавить в таблицу
+	}
+}
+
+function searchVariables(inputStringVar){
+	var variablesArr = [];
+	var stringLength = inputStringVar.length;
+	for(var i = 0; i < stringLength; i++){
+		if(inputStringVar[i].match(/([A-Z])/)){
+			if(variablesArr.indexOf(inputStringVar[i]) == -1){
+				variablesArr.push(inputStringVar[i]);
+			}
+		}
+	}
+	return variablesArr;
+}
+
+function drawInputTable(variablesArr, inputStringVar){
+	var tbody = document.getElementById('TableBody');
+	var row1 = document.createElement("tr");
+	var row2 = document.createElement("tr");
+	for(var i = 0; i < variablesArr.length; i++){
+		//tbody.append('<tr><td>' + variablesArr[i] + '</td><td><input type="number" id="' + i + '"></td></tr>');
+		var cell = document.createElement("td");
+        cell.innerHTML = variablesArr[i];
+		row1.appendChild(cell);
+	}
+	for(var i = 0; i < variablesArr.length; i++){
+		var cell = document.createElement("td");
+        cell.innerHTML = '<input type="number" min=0 max=1 id="input' + i + '">';
+		row2.appendChild(cell);
+	}
+	tbody.appendChild(row1);
+	tbody.appendChild(row2);
+	
+	continueButton = document.createElement("button");
+	continueButton.innerHTML = 'Рассчитать возможные значения формулы';
+	document.getElementById('continueButtonDiv').appendChild(continueButton);
+	var stringWithPartialValues = inputStringVar;
+	continueButton.onclick = function() {
+		
+		
+		var redusedVarArr = [];
+		for(var i = 0; i < variablesArr.length; i++){
+		var inputValue = document.getElementById('input' + i).value;
+		if(inputValue){
+			inputValue = parseInt(inputValue);
+			//stringWithPartialValues.replace("A", '1');
+			stringWithPartialValues = stringWithPartialValues.replace(variablesArr[i], inputValue);
+			} else {
+				redusedVarArr.push(variablesArr[i]);
+			}
+		}
+		
+		var tbody = document.getElementById('TableBody');
+		tbody.innerHTML = '';
+		document.getElementById('continueButtonDiv').innerHTML = '';
+		createHeadline(tbody, redusedVarArr);
+		var numOfCombinations = Math.pow(2, redusedVarArr.length);
+		for(var i = 0; i < numOfCombinations; i++){
+			var values = i.toString(2);
+			while(values.length < redusedVarArr.length){
+				values = '0' + values;
+			}
+			var stringWithValues = stringWithPartialValues;
+			for(j = 0; j < redusedVarArr.length; j++){
+				stringWithValues = stringWithValues.replace(redusedVarArr[j], values[j]);
+			}
+			var value = calculateValue(stringWithValues);
+			addInResult(tbody, values, value, redusedVarArr);
+		}
+	};
+}
+
+function addInResult(tbody, values, value, redusedVarArr){
+	var row = document.createElement("tr");
+	for(var i = 0; i < redusedVarArr.length; i++){
+		var cell = document.createElement("td");
+		cell.innerHTML = values[i];
+		row.appendChild(cell);
+		//tbody.appendChild(row);
+	}
+	var cell = document.createElement("td");
+    cell.innerHTML = value;
+	row.appendChild(cell);
+	tbody.appendChild(row);
+}
+
+function createHeadline(tbody, redusedVarArr){
+	var row = document.createElement("tr");
+	for(var i = 0; i < redusedVarArr.length; i++){
+		var cell = document.createElement("td");
+        cell.innerHTML = redusedVarArr[i];
+		row.appendChild(cell);
+		//tbody.appendChild(row);
+	}
+	var cell = document.createElement("td");
+    cell.innerHTML = 'Результат';
+	row.appendChild(cell);
+	tbody.appendChild(row);
+}
+
+function calculateValue(stringWithValues){
+	var unary_formula = /\(\!([01])\)/g;
+    var binary_formula = /\(([01])([\&\|\~]|(\-\>))([01])\)/g;
+	var conjunction = /\(([01])(\&)([01])\)/g;
+	var disjunction = /\(([01])(\|)([01])\)/g;
+	var implication = /\(([01])(\-\>)([01])\)/g;
+	var equivalence = /\(([01])(\~)([01])\)/g;
+
+    while(stringWithValues.match(unary_formula) || stringWithValues.match(binary_formula)){
+        if(stringWithValues.match(unary_formula)){
+			var match = stringWithValues.match(unary_formula);
+			for(var i = 0; i < match.length; i++){
+				stringWithValues = stringWithValues.replace(match[i], unary_formula_result(match[i]));
+			}
+		} else if(stringWithValues.match(conjunction)){
+			var match = stringWithValues.match(conjunction);
+			for(var i = 0; i < match.length; i++){
+				stringWithValues = stringWithValues.replace(match[i], conjunction_result(match[i]));
+			}
+		} else if(stringWithValues.match(disjunction)){
+			var match = stringWithValues.match(disjunction);
+			for(var i = 0; i < match.length; i++){
+				stringWithValues = stringWithValues.replace(match[i], disjunction_result(match[i]));
+			}
+		} else if(stringWithValues.match(implication)){
+			var match = stringWithValues.match(implication);
+			for(var i = 0; i < match.length; i++){
+				stringWithValues = stringWithValues.replace(match[i], implication_result(match[i]));
+			}
+		} else if(stringWithValues.match(equivalence)){
+			var match = stringWithValues.match(equivalence);
+			for(var i = 0; i < match.length; i++){
+				stringWithValues = stringWithValues.replace(match[i], equivalence_result(match[i]));
+			}
+		}
+    }
+	
+	return stringWithValues;
+}
+
+function unary_formula_result(match){
+	if(match.match(/\(\!([1])\)/g)){
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+function conjunction_result(match){
+	if(match.match(/\(([1])(\&)([1])\)/g)){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+function disjunction_result(match){
+	if(match.match(/\(([0])(\|)([0])\)/g)){
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+function implication_result(match){
+	if((match.match(/\(([0])(\-\>)([01])\)/g))||(match.match(/\(([1])(\-\>)([1])\)/g))){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+function equivalence_result(match){
+	if((match.match(/\(([0])(\~)([0])\)/g))||(match.match(/\(([1])(\~)([1])\)/g))){
+		return 1;
+	} else {
+		return 0;
+	}
+}
